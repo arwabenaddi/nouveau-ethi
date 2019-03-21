e3cb9765f4d4bbc3b30e02f112f7e77881b46b4f
arwabenaddi <benaddi.arwa@gmail.com>
2019-03-21 17:58:45 +0100

    first commit

---
+++ Listing.php
@@ -0,0 +1,40 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Variable cryptée</p>'; 
+ 
+    const METHOD = 'aes-128-cbc-hmac-sha1';
+
+     function simple_encrypt($message, $key)
+    {
+        if (mb_strlen($key, '8bit') !==16) {
+            throw new Exception("Needs a 256-bit key!");
+        }
+        $ivsize = openssl_cipher_iv_length(METHOD);
+        $iv = openssl_random_pseudo_bytes($ivsize);
+
+        $ciphertext = openssl_encrypt(
+            $message,
+            METHOD,
+            $key,
+            OPENSSL_RAW_DATA,
+            $iv
+        );
+
+        return  $iv . $ciphertext;
+    }
+
+ 
+ $id='6781#2018-02-14 16:33:39';
+ $variable_crypt=simple_encrypt($id, $salt = 'ODkwMDY1OS4zMTU=');
+$parametres = ['contexte'=>$variable_crypt,
+               'langue'=>'fr=FR',
+              ];
+ echo'<a  href="http://127.0.0.1:8000/listing?'.http_build_query($parametres).'">listing</a> <br>';
+ ?>
+ </body>
+</html>
+
+
